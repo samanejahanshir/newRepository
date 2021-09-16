78d897f0b0a87ce8d35a76c7171ffb8df2d93c36
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-16 13:42:29 +0430

    add file goodbye

---
+++ src/com/jahanshir/sm/resturant/Goodbye.java
@@ -0,0 +1,7 @@
+package com.jahanshir.sm.resturant;
+
+public class Goodbye {
+    public void  print(){
+        System.out.println("Goodbye");
+    }
+}
---
+++ src/com/jahanshir/sm/resturant/Main.java
@@ -0,0 +1,9 @@
+package com.jahanshir.sm.resturant;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+    }
+}
